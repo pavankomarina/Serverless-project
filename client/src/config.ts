b24fd3d3d7c4eacca09d408e2389350c34226cdf
Pavan <pavan@cyware.com>
2020-05-15 16:25:32 +0530

    Push code onto server

---
+++ client/src/config.ts
@@ -0,0 +1,10 @@
+
+const apiId = 'f8p90mgax8'
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+
+export const authConfig = {
+  
+  domain: 'dev-ec2pimkw.auth0.com',            // Auth0 domain
+  clientId: 'lalV4rfeKvUSz04hv4QgM8inw83XbX4V',          // Auth0 client id
+  callbackUrl: 'http://localhost:3000/callback'
+}
